0eaa9de0c2ebc4cf79021b7d3e623932abab2eba
Ivan Compagnucci <ivan@MacBook-Pro-di-Ivan.local>
2024-11-07 19:56:00 +0100

    Added time features

--- last_commit.sh
+++ last_commit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "last_updated: \"$(git log -1 --format="%cd" --date=iso)\"" > _data/last_commit.yml
+echo "last_updated: \"$(git log -1 --format='%cd' --date=format-local:%d\ %B\ %Y\ %H:%M\ \(UTC\))\"" > _data/last_commit.yml
